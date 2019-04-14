d2cdc354cbc7799ccbd1eb9e572e82a6bd9f6ce1
Denis <den.aryakhov@gmail.com>
2019-04-14 19:31:32 +0300

    Создана модель пользователя

---
+++ CodeBlogFitness.BL/Model/Gender.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CodeBlogFitness.BL.Model
+{
+    /// <summary>
+    ///  Пол.
+    /// </summary>
+    public class Gender
+    {
+        /// <summary>
+        /// Название.
+        /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        ///  Создать новый пол.
+        /// </summary>
+        /// <param name="name"> Имя пола </param>
+        public Gender(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("Имя пользователя не может быть пустым", nameof(name));
+            }
+
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
